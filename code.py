6dd4095cd4cc45104df7d2ceedc6df3aeb325b04
Petr Urbánek <514122@mail.muni.cz>
2022-04-26 16:41:21 +0200

    some fixed

--- code.py
+++ code.py
@@ -6,34 +6,39 @@ import flask
 
 
 def transcode_file(request, filename):
+    """some docstring"""
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=file)
     subprocess.call(command, shell=True)
 
 
 def load_config(filename):
+    """some docstring"""
     # Load a configuration file into YAML
     stream = file.open(filename, "w")
     config = yaml.load(stream)
 
 
 def authenticate(password):
+    """some docstring"""
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
 
 
 def fetch_website(urllib_version, url):
+    """some docstring"""
     # Import the requested version of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
     http = urllib.PoolManager()
-    r = http.request('GET', url)
-    return r.data
+    result = http.request('GET', url)
+    return result.data
 
 
 
 @app.route("/")
 def index():
+    """some docstring"""
     version = flask.request.args.get("urllib_version")
     url = flask.request.args.get("url")
     return fetch_website(version, url)
